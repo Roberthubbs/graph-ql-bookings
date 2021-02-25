1fcfa8470f7cb09dcded93fbc74513a7a5a67d1a
Robert Hubert <robertfhubert@gmail.com>
2021-02-24 21:28:22 -0500

    fix typo in launch file

--- start/server/src/datasources/launch.js
+++ start/server/src/datasources/launch.js
@@ -6,7 +6,7 @@ class LaunchAPI extends RESTDataSource {
     }
     async getAllLaunches() {
         const response = await this.get('launches');
-        return Array.isArray(response) ? response.map((launch) => this.launchReducer(launch));
+        return Array.isArray(response) ? [] : response.map((launch) => this.launchReducer(launch));
     }
 };
 
